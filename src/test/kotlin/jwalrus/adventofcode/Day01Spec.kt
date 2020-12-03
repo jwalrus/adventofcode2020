35879686ee3f31ed423018dd7237ab9a1f22bd13
jwalrus <jwalrus@protonmail.com>
2020-12-02 19:58:39 -0500

    revisit day 1 solutions and make a more efficient implementation

--- src/test/kotlin/jwalrus/adventofcode/Day01Spec.kt
+++ src/test/kotlin/jwalrus/adventofcode/Day01Spec.kt
@@ -29,4 +29,26 @@ class Day01Spec : DescribeSpec ({
             challenge.part2(2020) shouldBeExactly 295_668_576
         }
     }
+
+    describe("part 1 (take 2)") {
+
+        it("sample data = 514,579") {
+            sample.part1take2(2020)!! shouldBeExactly 514_579
+        }
+
+        it("challenge data = 928,896") {
+            challenge.part1take2(2020)!! shouldBeExactly 928_896
+        }
+    }
+
+    describe("part 2 (take 2)") {
+
+        it("sample data = 241,861,950") {
+            sample.part2take2(2020)!! shouldBeExactly 241_861_950
+        }
+
+        it("challenge data = 295,668,576") {
+            challenge.part2take2(2020)!! shouldBeExactly 295_668_576
+        }
+    }
 })
