05e3667182efa7d63755eed385cf80fb72b8b19f
jwalrus <jwalrus@protonmail.com>
2020-12-05 11:31:47 -0500

    clean up day 1 code

--- src/main/kotlin/jwalrus/adventofcode/Day01.kt
+++ src/main/kotlin/jwalrus/adventofcode/Day01.kt
@@ -7,42 +7,48 @@ import arrow.core.k
 /* **************************
  * Take 1: Using ListK Monad
  * **************************/
-fun List<Int>.part1(target: Int): Int = this.k().run {
-    ListK.fx {
-        val x = !this@run
-        val y = !this@run.filter { it != x && x + it == target }.k()
-        x to y
-    }.first().let { (x, y) -> x * y }
-}
+object Day01Monad {
 
-// stupid inefficient
-fun List<Int>.part2(target: Int): Int = this.k().run {
-    ListK.fx {
-        val x = !this@run
-        val y = !this@run.filter { it != x }.k()
-        val z = !this@run.filter { it != x && it != y && x + y + it == target }.k()
-        Triple(x, y, z)
-    }.first().let { (x, y, z) -> x * y * z }
+    fun List<Int>.part1(target: Int): Int = this.k().run {
+        ListK.fx {
+            val x = !this@run
+            val y = !this@run.filter { it != x && x + it == target }.k()
+            x to y
+        }.first().let { (x, y) -> x * y }
+    }
+
+    // stupid inefficient
+    fun List<Int>.part2(target: Int): Int = this.k().run {
+        ListK.fx {
+            val x = !this@run
+            val y = !this@run.filter { it != x }.k()
+            val z = !this@run.filter { it != x && it != y && x + y + it == target }.k()
+            Triple(x, y, z)
+        }.first().let { (x, y, z) -> x * y * z }
+    }
 }
 
 /* *************************************************
  * Take 2: More efficient kotlin-only implementation
  * *************************************************/
-private fun List<Int>.go_(target: Int): Int? = asSequence()
-    .firstOrNull { binarySearch(target - it) > -1 }
-    ?.let { it * (target - it) }
+object Day01 {
 
-fun List<Int>.part1take2(target: Int): Int? = sorted().go_(target)
+    private fun List<Int>.go_(target: Int): Int? = asSequence()
+        .firstOrNull { binarySearch(target - it) > -1 }
+        ?.let { it * (target - it) }
 
-fun List<Int>.part2take2(target: Int): Int? {
-    tailrec fun go(x: Int, xs: List<Int>): Int? = when (val result = xs.go_(target - x)) {
-        null -> go(xs[0], xs.drop(1))
-        else -> x * result
-    }
+    fun List<Int>.part1(target: Int): Int? = sorted().go_(target)
 
-    return when (isNotEmpty()) {
-        true -> sorted().let { go(it[0], it.drop(1)) }
-        false -> null
+    fun List<Int>.part2(target: Int): Int? {
+        tailrec fun go(x: Int, xs: List<Int>): Int? = when (val result = xs.go_(target - x)) {
+            null -> go(xs[0], xs.drop(1))
+            else -> x * result
+        }
+
+        return when (isNotEmpty()) {
+            true -> sorted().let { go(it[0], it.drop(1)) }
+            false -> null
+        }
     }
 }
 
--- src/test/kotlin/jwalrus/adventofcode/Day01Spec.kt
+++ src/test/kotlin/jwalrus/adventofcode/Day01Spec.kt
@@ -2,6 +2,8 @@ package jwalrus.adventofcode
 
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.ints.shouldBeExactly
+import jwalrus.adventofcode.Day01.part1
+import jwalrus.adventofcode.Day01.part2
 
 class Day01Spec : DescribeSpec ({
 
@@ -11,44 +13,44 @@ class Day01Spec : DescribeSpec ({
     describe("part 1") {
 
         it("sample data = 514,579") {
-            sample.part1(2020) shouldBeExactly 514_579
+            Day01Monad.run { sample.part1(2020) } shouldBeExactly 514_579
         }
 
         it("challenge data = 928,896") {
-            challenge.part1(2020) shouldBeExactly 928_896
+            Day01Monad.run { challenge.part1(2020) } shouldBeExactly 928_896
         }
     }
 
     describe("part 2") {
 
         it("sample data = 241,861,950") {
-            sample.part2(2020) shouldBeExactly 241_861_950
+            Day01Monad.run { sample.part2(2020) } shouldBeExactly 241_861_950
         }
 
         it("challenge data = 295,668,576") {
-            challenge.part2(2020) shouldBeExactly 295_668_576
+            Day01Monad.run { challenge.part2(2020) } shouldBeExactly 295_668_576
         }
     }
 
     describe("part 1 (take 2)") {
 
         it("sample data = 514,579") {
-            sample.part1take2(2020)!! shouldBeExactly 514_579
+            sample.part1(2020)!! shouldBeExactly 514_579
         }
 
         it("challenge data = 928,896") {
-            challenge.part1take2(2020)!! shouldBeExactly 928_896
+            challenge.part1(2020)!! shouldBeExactly 928_896
         }
     }
 
     describe("part 2 (take 2)") {
 
         it("sample data = 241,861,950") {
-            sample.part2take2(2020)!! shouldBeExactly 241_861_950
+            sample.part2(2020)!! shouldBeExactly 241_861_950
         }
 
         it("challenge data = 295,668,576") {
-            challenge.part2take2(2020)!! shouldBeExactly 295_668_576
+            challenge.part2(2020)!! shouldBeExactly 295_668_576
         }
     }
 })
