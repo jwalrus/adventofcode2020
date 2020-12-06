5c6b9a8eceebc34dd9d4a027a93c19be11ad5572
jwalrus <jwalrus@protonmail.com>
2020-12-06 14:02:26 -0500

    move resources

--- src/main/kotlin/jwalrus/adventofcode/util/DataUtil.kt
+++ src/main/kotlin/jwalrus/adventofcode/util/DataUtil.kt
@@ -3,10 +3,10 @@ package jwalrus.adventofcode.util
 import java.nio.file.Paths
 
 fun load(name: String): List<String>
-        = Paths.get("src", "test", "resources", name).toFile().readLines()
+        = Paths.get("src", "main", "resources", name).toFile().readLines()
 
 fun loadText(name: String): String
-        = Paths.get("src", "test", "resources", name).toFile().readText()
+        = Paths.get("src", "main", "resources", name).toFile().readText()
 
 fun loadGroups(name: String): List<List<String>>
         = loadText(name).split("\n\n").map { it.trim().split("\n") }
