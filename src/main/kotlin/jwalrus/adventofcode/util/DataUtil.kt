5bca2bd3b306d44e5f8c9b55c23eadfc102d8e4c
jwalrus <jwalrus@protonmail.com>
2020-12-07 18:55:58 -0500

    add parsers that uses arrow's traverse typeclass

--- src/main/kotlin/jwalrus/adventofcode/util/DataUtil.kt
+++ src/main/kotlin/jwalrus/adventofcode/util/DataUtil.kt
@@ -1,5 +1,8 @@
 package jwalrus.adventofcode.util
 
+import arrow.core.*
+import arrow.core.extensions.either.applicative.applicative
+import arrow.core.extensions.list.traverse.traverse
 import java.nio.file.Paths
 
 fun load(name: String): List<String>
@@ -10,3 +13,15 @@ fun loadText(name: String): String
 
 fun loadGroups(name: String): List<List<String>>
         = loadText(name).split("\n\n").map { it.trim().split("\n") }
+
+/**
+ * Use [traverse] from Arrow under the hood. Example usage:
+ * ```
+ *
+ * load("day01_data.txt") { s ->
+ *      s.toIntOrNull()?.right() ?: "$s cannot be parsed to int!".left()
+ * } // Right(ListK(123, 456, 8574, ...))
+ */
+fun <T> load(name: String, f: (String) -> Either<String, T>): Either<String, ListK<T>> {
+    return load(name).k().traverse(Either.applicative()) { f(it) }.fix()
+}
