7a7b30f19702e04ce05f7cd0c7714ab60d217c7c
jwalrus <jwalrus@protonmail.com>
2020-12-10 19:30:40 -0500

    cleanup

--- src/main/kotlin/jwalrus/adventofcode/Day02.kt
+++ src/main/kotlin/jwalrus/adventofcode/Day02.kt
@@ -1,20 +1,20 @@
 package jwalrus.adventofcode
 
-import arrow.core.Tuple4
-
-private typealias Row = Tuple4<Int, Int, Char, String>
-
-private val regex: Regex = "^(\\d+)-(\\d+) ([A-Za-z]): (.+)$".toRegex()
-
-private fun String.toRow(): Row = regex.find(this)!!.destructured
-    .let { (a, b, c, d) -> Tuple4(a.toInt(), b.toInt(), c.toCharArray()[0], d) }
-
-private fun String.containsCharBetween(c: Char, lower: Int, upper: Int): Boolean = count { it == c } in lower..upper
-
-private fun String.containsCharAtIndexExactlyOnce(c: Char, ix: Set<Int>): Boolean =
-    filterIndexed { index, char -> (index + 1) in ix && char == c }.length == 1
+import jwalrus.adventofcode.util.load
 
 object Day02 {
+
+    private data class Row(val a: Int, val b: Int, val c: Char, val d: String)
+
+    private val regex: Regex = "^(\\d+)-(\\d+) ([A-Za-z]): (.+)$".toRegex()
+
+    private fun String.toRow(): Row = regex.find(this)!!.destructured
+        .let { (a, b, c, d) -> Row(a.toInt(), b.toInt(), c.toCharArray()[0], d) }
+
+    private fun String.containsCharBetween(c: Char, lower: Int, upper: Int): Boolean = count { it == c } in lower..upper
+
+    private fun String.containsCharAtIndexExactlyOnce(c: Char, ix: Set<Int>): Boolean =
+        filterIndexed { index, char -> (index + 1) in ix && char == c }.length == 1
 
     fun List<String>.part1(): Int = asSequence().map { it.toRow() }
         .filter { tup -> tup.d.containsCharBetween(tup.c, tup.a, tup.b) }
@@ -23,4 +23,15 @@ object Day02 {
     fun List<String>.part2(): Int = asSequence().map { it.toRow() }
         .filter { tup -> tup.d.containsCharAtIndexExactlyOnce(tup.c, setOf(tup.a, tup.b)) }
         .count()
+}
+
+fun main() = Day02.run {
+    val sample = listOf("1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc")
+    val challenge = load("day02_data.txt")
+
+    println(sample.part1()) // 2
+    println(challenge.part1()) // 500
+
+    println(sample.part2()) // 1
+    println(challenge.part2()) // 313
 }
--- src/main/kotlin/jwalrus/adventofcode/Day03.kt
+++ src/main/kotlin/jwalrus/adventofcode/Day03.kt
@@ -1,5 +1,7 @@
 package jwalrus.adventofcode
 
+import jwalrus.adventofcode.util.load
+
 class SledMap(private val map: List<String>) {
 
     private val height = map.size
@@ -15,5 +17,24 @@ class SledMap(private val map: List<String>) {
 
     fun part1(down: Int, right: Int): Long = treesOnPath(down, right)
 
-    fun part2(slopes: List<Pair<Int, Int>>): Long = slopes.map { (x,y) -> treesOnPath(x,y) }.reduce(Long::times)
+    fun part2(slopes: List<Pair<Int, Int>>): Long = slopes.map { (x, y) -> treesOnPath(x, y) }.reduce(Long::times)
+}
+
+fun main() {
+    val sample = load("day03_data_sample.txt")
+    val challenge = load("day03_data.txt")
+
+    println(SledMap(sample).part1(1, 3)) // 7
+    println(SledMap(challenge).part1(1, 3)) // 289
+
+    val slopes = listOf(
+        1 to 1,
+        1 to 3,
+        1 to 5,
+        1 to 7,
+        2 to 1
+    )
+
+    println(SledMap(sample).part2(slopes)) // 336
+    println(SledMap(challenge).part2(slopes)) // 5_522_401_584
 }
